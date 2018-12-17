671a6066ac986ef9b5a0e56e12928edceda15378
Carlos Moya <carlosmoya@gmail.com>
2018-12-17 20:01:29 -0300

    Version inicial del proyecto final
    Una estructura para que todos arranquemos de la misma.

---
+++ src/main/java/com/gurpo_1/servicel/ServicelApplication.java
@@ -0,0 +1,14 @@
+package com.gurpo_1.servicel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicelApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gurpo_1/servicel/ServicelApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gurpo_1.servicel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
